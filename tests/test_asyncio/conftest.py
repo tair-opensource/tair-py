bcc4dceb0c4d53257861e72b7c79aaf7b55993b5
Vincil Lau <vincillau@outlook.com>
2022-08-12 11:54:50 +0800

    feat: import configure from conftest.py

--- tests/test_asyncio/conftest.py
+++ tests/test_asyncio/conftest.py
@@ -1,10 +1,8 @@
 import pytest_asyncio
 from datetime import datetime
 from tair.asyncio import Tair
+from ..conftest import TAIR_HOST, TAIR_PORT, TAIR_DB, TAIR_USERNAME, TAIR_PASSWORD
 
-DEFAULT_TAIR_HOST = "localhost"
-DEFAULT_TAIR_PORT = 6379
-DEFAULT_TAIR_DB = 0
 
 # due to network delay, ttl and pttl are not very accurate,
 # so we set a calibration value.
@@ -14,11 +12,11 @@ NETWORK_DELAY_CALIBRATION_VALUE = 1000
 
 async def get_tair_client() -> Tair:
     tair = Tair(
-        host=DEFAULT_TAIR_HOST,
-        port=DEFAULT_TAIR_PORT,
-        db=DEFAULT_TAIR_DB,
-        username="root",
-        password="123456",
+        host=TAIR_HOST,
+        port=TAIR_PORT,
+        db=TAIR_DB,
+        username=TAIR_USERNAME,
+        password=TAIR_PASSWORD,
     )
     await tair.initialize()
     return tair
