86bb982f19c51d166214764a50e59146da53ebbc
Vincil Lau <vincillau@outlook.com>
2022-08-26 12:06:20 +0800

    feat: test cluster in github action

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,10 @@
+import os
 from datetime import datetime
+from typing import Union
 
 import pytest
 
-from tair import Tair
+from tair import Tair, TairCluster
 
 # change the following configuration for your Tair.
 TAIR_HOST = "localhost"
@@ -11,21 +13,43 @@ TAIR_DB = 0
 TAIR_USERNAME = None
 TAIR_PASSWORD = None
 
+TAIR_CLUSERT_HOST = "localhost"
+TAIR_CLUSERT_PORT = 30001
+TAIR_CLUSERT_USERNAME = None
+TAIR_CLUSERT_PASSWORD = None
+
 # due to network delay, ttl and pttl are not very accurate,
 # so we set a calibration value.
 # unit: millisecond
 NETWORK_DELAY_CALIBRATION_VALUE = 1000
 
 
-@pytest.fixture()
-def t() -> Tair:
-    tair = Tair(
+def get_tair_client() -> Tair:
+    return Tair(
         host=TAIR_HOST,
         port=TAIR_PORT,
         db=TAIR_DB,
         username=TAIR_USERNAME,
         password=TAIR_PASSWORD,
     )
+
+
+def get_tair_cluster_client() -> TairCluster:
+    return TairCluster(
+        host=TAIR_CLUSERT_HOST,
+        port=TAIR_CLUSERT_PORT,
+        username=TAIR_CLUSERT_USERNAME,
+        password=TAIR_CLUSERT_PASSWORD,
+    )
+
+
+# set TEST_TAIR_CLUSTER environment variable to enable cluster mode.
+@pytest.fixture()
+def t() -> Union[Tair, TairCluster]:
+    if os.environ.get("TEST_TAIR_CLUSTER") is None:
+        tair = get_tair_client()
+    else:
+        tair = get_tair_cluster_client()
     yield tair
     tair.close()
 
